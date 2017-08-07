eadf45235b6a30032261754c4ce7e6ffc3c4014d
Peter Bierman <peter@milestone-development.nl>
2017-08-07 19:58:39 +0200

    Removed unneccessay code

--- www/webintent.js
+++ www/webintent.js
@@ -8,69 +8,19 @@
 
     };
 
-    WebIntent.prototype.ACTION_SEND = "android.intent.action.SEND";
-    WebIntent.prototype.ACTION_VIEW= "android.intent.action.VIEW";
-    WebIntent.prototype.EXTRA_TEXT = "android.intent.extra.TEXT";
-    WebIntent.prototype.EXTRA_SUBJECT = "android.intent.extra.SUBJECT";
-    WebIntent.prototype.EXTRA_STREAM = "android.intent.extra.STREAM";
-    WebIntent.prototype.EXTRA_EMAIL = "android.intent.extra.EMAIL";
-    WebIntent.prototype.ACTION_CALL = "android.intent.action.CALL";
-    WebIntent.prototype.ACTION_SENDTO = "android.intent.action.SENDTO";
-
-    WebIntent.prototype.startActivity = function(params, success, fail) {
+    WebIntent.prototype.getIntent = function(success, fail) {
         return cordova.exec(function(args) {
             success(args);
         }, function(args) {
             fail(args);
-        }, 'WebIntent', 'startActivity', [params]);
+        }, 'WebIntent', 'getIntent', []);
     };
-
-    WebIntent.prototype.hasExtra = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'hasExtra', [params]);
-    };
-
-    WebIntent.prototype.getUri = function(success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'getUri', []);
-    };
-
-    WebIntent.prototype.getUrl = function(success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'getUrl', []);
-    };
-
-    WebIntent.prototype.getExtra = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'getExtra', [params]);
-    };
-
 
     WebIntent.prototype.onNewIntent = function(callback) {
         return cordova.exec(function(args) {
             callback(args);
         }, function(args) {
         }, 'WebIntent', 'onNewIntent', []);
-    };
-
-    WebIntent.prototype.sendBroadcast = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'sendBroadcast', [params]);
     };
 
     window.webintent = new WebIntent();
